299745a20402170fc98636d83299ce55ddb3fff4
Sergio Andrés SÁNCHEZ <103612998+SergioAndresSanchezTdfEdu@users.noreply.github.com>
2023-12-17 20:52:47 -0300

    Feature/t8 s3 76 (#11)
    * feature/T8S3-76 sin funcionalidad
    
    * feature/T8S3-76

--- public/home.js
+++ public/home.js
@@ -1,2 +1,8 @@
 let userName = document.getElementById('userName');
 userName.textContent = sessionStorage.getItem("userName");
+
+let logout = document.getElementById('Logout');
+
+logout.addEventListener("click", async () => {
+    sessionStorage.removeItem("token");
+});
