294026af22d4b0d0c5c892f28681bb52d6678735
leorivero <leorivero@live.com.ar>
2023-12-17 22:29:02 -0300

    check

--- public/gastos/gastos.js
+++ public/gastos/gastos.js
@@ -1,14 +1,15 @@
 const inputAmount = document.getElementById("amount");
 const inputDescription = document.getElementById("description");
 const btnSubmit = document.getElementById("btnSubmit");
-const date= document.getElementById("date");
+const inputDate= document.getElementById("date");
 
 btnSubmit.addEventListener("click", async (e) => {
 
   console.log("click");
   const amount = inputAmount.value;
   const description = inputDescription.value;
-  console.log(amount, description);
+  const date= inputDate.value;
+  console.log(amount, description,date);
   if (amount>0) {
     e.preventDefault();
     try {
@@ -16,31 +17,12 @@ btnSubmit.addEventListener("click", async (e) => {
         amount:amount,
         currency:"ARS",
         description:description,
+        date: date,
         token:sessionStorage.getItem("token")
       });
-      console.log(response.data);
+    console.log(response.data);
     } catch (error) {
       console.log(error);
     }
   }
 });
-
-btnClose.onclick = function() {
-  modal.style.display = 'none';
-}
-
-window.onclick = function(event) {
-  if (event.target === modal) {
-      modal.style.display = 'none';
-  }
-}
-
-const getDate = (transactionDate) => {
-  const date = transactionDate.slice(0, 10);
-  const dateComponents = date.split("-");
-  const d = dateComponents[2];
-  const m = dateComponents[1];
-  const y = dateComponents[0];
-  const time = transactionDate.slice(11, 19);
-  return d + "-" + m + "-" + y + " " + time;
-};
